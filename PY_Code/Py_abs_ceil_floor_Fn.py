7036c326d00cda55e86fd6f99efc01053eea6922
vijay <vijayjeyaram@gmail.com>
2019-08-11 19:23:14 +0530

    abs_Ceil_Floor file description has been updated.

--- PY_Code/Py_abs_ceil_floor_Fn.py
+++ PY_Code/Py_abs_ceil_floor_Fn.py
@@ -1,7 +1,7 @@
 # Absolute Function
 """
 The abs() method returns the absolute value of x i.e.
-the positive DISTANCE between x and zero.
+the positive DISTANCE between x/-x and zero.
 """
 x = 10
 print(abs(x))
