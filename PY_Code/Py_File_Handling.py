8664b97c5b3e35055913eeeda28ee1146402c54b
vijay <vijayjeyaram@gmail.com>
2019-08-11 21:22:16 +0530

    Python File Handling Script file has been created.

---
+++ PY_Code/Py_File_Handling.py
@@ -0,0 +1,20 @@
+# Text File Handling
+
+# Create a Text file
+myTextFile = open("/Users/vijay/Documents/MyWorkPlace/Learning/Python/PyCharm_PY_WorkPlace/External_Files/TextFile.txt", 'w')
+myTextFile.write("Python Text File.")
+myTextFile.close()
+
+# Append a Text file
+myTextFile = open("/Users/vijay/Documents/MyWorkPlace/Learning/Python/PyCharm_PY_WorkPlace/External_Files/TextFile.txt", 'a')
+myTextFile.write("This File Contains description of python methods. ")
+myTextFile.close()
+
+# Read a Text file
+myTextFile = open("/Users/vijay/Documents/MyWorkPlace/Learning/Python/PyCharm_PY_WorkPlace/External_Files/TextFile.txt", 'r')
+view_Content = myTextFile.read()
+print(view_Content)
+myTextFile.close()
+
+
+
