aa1e9f71da9f6a8b378cdacd3ad8726c79531ce1
vijay <vijayjeyaram@gmail.com>
2019-08-12 19:13:24 +0530

    Accessing Python User Defined Module.

---
+++ PY_Code/Py_Use_Calculator_Module.py
@@ -0,0 +1,7 @@
+from PY_Code.Py_Calculator_Module import *
+
+print("Calculator Module Add", add(5, 5))
+print("Calculator Module Sub", sub(25, 5))
+print("Calculator Module Mul", mul(5, 5))
+print("Calculator Module Div()", round(div(21, 7)))
+
