9a5b3f09242fd6f95a0a3534977f44f2c032f0dc
vijay <vijayjeyaram@gmail.com>
2019-08-12 18:13:21 +0530

    Python Exception handling script has been created.

---
+++ PY_Code/Py_Exception_Handling.py
@@ -0,0 +1,13 @@
+# Exception Handling
+
+# List Without Exception
+myList = [1, 2, 3, 4, 5]
+print(myList)
+print(myList[2])
+
+# List With Exception
+try:
+    print(myList[7])
+except Exception as e:
+    print("Entered Value crossed the Boundary.", e)
+
