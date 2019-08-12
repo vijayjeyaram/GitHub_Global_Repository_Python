9a1b4c926d2fb50ffa7865a3d5831220bebf8cc9
vijay <vijayjeyaram@gmail.com>
2019-08-12 19:12:58 +0530

    Python User Defined Module script has been created.

---
+++ PY_Code/Py_Calculator_Module.py
@@ -0,0 +1,18 @@
+# Calculator Module
+
+# Addition Function
+def add(x, y):
+    return x + y
+
+
+# Subtraction Function
+def sub(x, y):
+    return x - y
+
+# Multiplication Function
+def mul(x, y):
+    return x * y
+
+# Division Function
+def div(x, y):
+    return x / y
