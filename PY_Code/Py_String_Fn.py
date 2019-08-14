6c170f97456588525167cba563bc07bad2870f52
vijay <vijayjeyaram@gmail.com>
2019-08-14 16:03:17 +0530

    Python Strip and digit Function script has been added.

--- PY_Code/Py_String_Fn.py
+++ PY_Code/Py_String_Fn.py
@@ -20,3 +20,24 @@ print("Encoded string:", proverb_02.encode())
 proverb_02 = "PRACTICE MAKES PERFECT"
 print("split string:", proverb_02.split(" "))
 
+# Replace():
+test_01 = "Hello World"
+print("Replace string:", test_01.replace("World", "Universe"))
+
+#IsDigit():
+test_02 = "12345.x"
+print("IsDigit string:", test_02.isdigit())
+
+#IsAlpha():
+test_03 = "x.1"
+print("IsAlpha string:", test_03.isalpha())
+
+#strip(): Using this function we can remove particular char from a string (left/right) side
+test_04 = "oooTestooo"
+print("remove particular char from a string:", test_04.strip("o"))
+
+#lstrip(): Using this function we can remove particular char from a string (left side)
+print("remove particular char from a string:", test_04.lstrip("o"))
+
+#rstrip(): Using this function we can remove particular char from a string (right side)
+print("remove particular char from a string:", test_04.rstrip("o"))
